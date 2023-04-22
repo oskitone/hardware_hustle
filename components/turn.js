596c80b83a7c83e17e00be6fbb693d299d7d4a2b
Tommy <tommy@iamnotagoodartist.com>
2023-04-22 10:33:18 -0700

    Remove dummy data

--- components/turn.js
+++ components/turn.js
@@ -16,13 +16,13 @@ export default function Turn({ id }) {
           <div class={`${styles.th} ${styles.iconCol}`}>
             <i className="bi bi-currency-dollar"></i>
           </div>
-          <div class={`${styles.td} ${styles.topLeft} ${styles.bottomLeft}`}>
-            2
-          </div>
+          <div
+            class={`${styles.td} ${styles.topLeft} ${styles.bottomLeft}`}
+          ></div>
           <div class={`${styles.td}`}></div>
-          <div class={`${styles.td} ${styles.topRight} ${styles.bottomRight}`}>
-            41
-          </div>
+          <div
+            class={`${styles.td} ${styles.topRight} ${styles.bottomRight}`}
+          ></div>
         </div>
       </div>
       <div class={`${styles.tbody}`}>
@@ -30,32 +30,32 @@ export default function Turn({ id }) {
           <div class={`${styles.th} ${styles.iconCol}`}>
             <i className="bi bi-bezier2"></i>
           </div>
-          <div class={`${styles.td} ${styles.topLeft}`}>5</div>
-          <div class={`${styles.td}`}>3</div>
+          <div class={`${styles.td} ${styles.topLeft}`}></div>
+          <div class={`${styles.td}`}></div>
           <div class={`${styles.td} ${styles.topRight}`}></div>
         </div>
         <div class={`${styles.tr}`}>
           <div class={`${styles.th} ${styles.iconCol}`}>
             <i className="bi bi-box-seam"></i>
           </div>
-          <div class={`${styles.td}`}>2</div>
-          <div class={`${styles.td}`}>1</div>
+          <div class={`${styles.td}`}></div>
+          <div class={`${styles.td}`}></div>
           <div class={`${styles.td}`}></div>
         </div>
         <div class={`${styles.tr}`}>
           <div class={`${styles.th} ${styles.iconCol}`}>
             <i className="bi bi-hdd-rack"></i>
           </div>
-          <div class={`${styles.td}`}>3</div>
-          <div class={`${styles.td}`}>2</div>
+          <div class={`${styles.td}`}></div>
+          <div class={`${styles.td}`}></div>
           <div class={`${styles.td}`}></div>
         </div>
         <div class={`${styles.tr}`}>
           <div class={`${styles.th} ${styles.iconCol}`}>
             <i className="bi bi-battery-half"></i>
           </div>
-          <div class={`${styles.td} ${styles.bottomLeft}`}>1</div>
-          <div class={`${styles.td}`}>0</div>
+          <div class={`${styles.td} ${styles.bottomLeft}`}></div>
+          <div class={`${styles.td}`}></div>
           <div class={`${styles.td} ${styles.bottomRight}`}></div>
         </div>
       </div>
@@ -65,14 +65,14 @@ export default function Turn({ id }) {
             <i className="bi bi-motherboard"></i>
           </div>
           <div class={`${styles.td} ${styles.topLeft}`}></div>
-          <div class={`${styles.td}`}>1</div>
-          <div class={`${styles.td} ${styles.topRight}`}>0</div>
+          <div class={`${styles.td}`}></div>
+          <div class={`${styles.td} ${styles.topRight}`}></div>
         </div>
         <div class={`${styles.tr}`}>
           <div class={`${styles.th} ${styles.iconCol}`}>
             <i className="bi bi-boombox"></i>
           </div>
-          <div class={`${styles.td}`}>2</div>
+          <div class={`${styles.td}`}></div>
           <div class={`${styles.td}`}></div>
           <div class={`${styles.td}`}></div>
         </div>
@@ -80,9 +80,9 @@ export default function Turn({ id }) {
           <div class={`${styles.th} ${styles.iconCol}`}>
             <i className="bi bi-truck-front"></i>
           </div>
-          <div class={`${styles.td} ${styles.bottomLeft}`}>1</div>
-          <div class={`${styles.td}`}>2</div>
-          <div class={`${styles.td} ${styles.bottomRight}`}>1</div>
+          <div class={`${styles.td} ${styles.bottomLeft}`}></div>
+          <div class={`${styles.td}`}></div>
+          <div class={`${styles.td} ${styles.bottomRight}`}></div>
         </div>
       </div>
     </div>
