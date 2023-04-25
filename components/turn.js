c1e69b4c49e95a42fbc371eb9c65fc8f492aad62
Tommy <tommy@iamnotagoodartist.com>
2023-04-25 15:54:05 -0700

    Reactify Turn table

--- components/turn.js
+++ components/turn.js
@@ -1,135 +1,129 @@
+import classnames from "classnames";
 import Icon from "components/icon";
 
 import styles from "@/styles/Turn.module.css";
 
+function Head({ children }) {
+  return <div class={`${styles.thead}`}>{children}</div>;
+}
+
+function Body({ children }) {
+  return <div class={`${styles.tbody}`}>{children}</div>;
+}
+
+function Row({ children }) {
+  return <div class={`${styles.tr}`}>{children}</div>;
+}
+
+function Cell({
+  icon,
+  children,
+
+  head,
+
+  carryOver,
+  final,
+
+  topLeft,
+  topRight,
+  bottomLeft,
+  bottomRight,
+}) {
+  return (
+    <div
+      contentEditable
+      className={classnames({
+        [styles.td]: !head && !icon,
+        [styles.th]: head,
+
+        [styles.carryOver]: carryOver,
+        [styles.final]: final,
+
+        [styles.topLeft]: topLeft,
+        [styles.topRight]: topRight,
+        [styles.bottomLeft]: bottomLeft,
+        [styles.bottomRight]: bottomRight,
+      })}
+    >
+      {icon && <Icon id={icon} />}
+      {children}
+    </div>
+  );
+}
+
 export default function Turn({ id }) {
   return (
     <div class={`${styles.turn}`}>
-      <div class={`${styles.thead}`}>
-        <div class={`${styles.tr}`}>
-          <div class={`${styles.id}`}>{id}</div>
-          <div class={`${styles.th}`}>Buy</div>
-          <div class={`${styles.th}`}>Make</div>
-          <div class={`${styles.th}`}>Sell</div>
-        </div>
-      </div>
-      <div class={`${styles.tbody}`}>
-        <div class={`${styles.tr}`}>
-          <div class={`${styles.th} ${styles.iconCol}`}>
-            <Icon id="money" />
-          </div>
-          <div
-            contentEditable
-            class={`${styles.td} ${styles.topLeft} ${styles.bottomLeft}`}
-          ></div>
-          <div class={`${styles.td} ${styles.carryover}`}></div>
-          <div
-            contentEditable
-            class={`${styles.td} ${styles.topRight} ${styles.bottomRight} ${styles.final}`}
-          ></div>
-        </div>
-      </div>
-      <div class={`${styles.tbody}`}>
-        <div class={`${styles.tr}`}>
-          <div class={`${styles.th} ${styles.iconCol}`}>
-            <Icon id="time" />
-          </div>
-          <div
-            contentEditable
-            class={`${styles.td} ${styles.topLeft} ${styles.bottomLeft}`}
-          ></div>
-          <div contentEditable class={`${styles.td}`}></div>
-          <div
-            contentEditable
-            class={`${styles.td} ${styles.topRight} ${styles.bottomRight} ${styles.final}`}
-          ></div>
-        </div>
-      </div>
-      <div class={`${styles.tbody}`}>
-        <div class={`${styles.tr}`}>
-          <div class={`${styles.th} ${styles.iconCol}`}>
-            <Icon id="A" />
-          </div>
-          <div contentEditable class={`${styles.td} ${styles.topLeft}`}></div>
-          <div contentEditable class={`${styles.td}`}></div>
-          <div
-            contentEditable
-            class={`${styles.td} ${styles.topRight} ${styles.final} ${styles.carryover}`}
-          ></div>
-        </div>
-        <div class={`${styles.tr}`}>
-          <div class={`${styles.th} ${styles.iconCol}`}>
-            <Icon id="B" />
-          </div>
-          <div contentEditable class={`${styles.td}`}></div>
-          <div contentEditable class={`${styles.td}`}></div>
-          <div
-            contentEditable
-            class={`${styles.td} ${styles.final} ${styles.carryover}`}
-          ></div>
-        </div>
-        <div class={`${styles.tr}`}>
-          <div class={`${styles.th} ${styles.iconCol}`}>
-            <Icon id="C" />
-          </div>
-          <div contentEditable class={`${styles.td}`}></div>
-          <div contentEditable class={`${styles.td}`}></div>
-          <div
-            contentEditable
-            class={`${styles.td} ${styles.final} ${styles.carryover}`}
-          ></div>
-        </div>
-        <div class={`${styles.tr}`}>
-          <div class={`${styles.th} ${styles.iconCol}`}>
-            <Icon id="D" />
-          </div>
-          <div
-            contentEditable
-            class={`${styles.td} ${styles.bottomLeft}`}
-          ></div>
-          <div contentEditable class={`${styles.td}`}></div>
-          <div
-            contentEditable
-            class={`${styles.td} ${styles.bottomRight} ${styles.final} ${styles.carryover}`}
-          ></div>
-        </div>
-      </div>
-      <div class={`${styles.tbody}`}>
-        <div class={`${styles.tr}`}>
-          <div class={`${styles.th} ${styles.iconCol}`}>
-            <Icon id="AB" />
-          </div>
-          <div
-            class={`${styles.td} ${styles.topLeft} ${styles.carryover}`}
-          ></div>
-          <div contentEditable class={`${styles.td}`}></div>
-          <div
-            contentEditable
-            class={`${styles.td} ${styles.topRight} ${styles.final}`}
-          ></div>
-        </div>
-        <div class={`${styles.tr}`}>
-          <div class={`${styles.th} ${styles.iconCol}`}>
-            <Icon id="ABC" />
-          </div>
-          <div class={`${styles.td} ${styles.carryover}`}></div>
-          <div contentEditable class={`${styles.td}`}></div>
-          <div contentEditable class={`${styles.td} ${styles.final}`}></div>
-        </div>
-        <div class={`${styles.tr}`}>
-          <div class={`${styles.th} ${styles.iconCol}`}>
-            <Icon id="ABCD" />
-          </div>
-          <div
-            class={`${styles.td} ${styles.bottomLeft} ${styles.carryover}`}
-          ></div>
-          <div contentEditable class={`${styles.td}`}></div>
-          <div
-            contentEditable
-            class={`${styles.td} ${styles.bottomRight} ${styles.final}`}
-          ></div>
-        </div>
-      </div>
+      <Head>
+        <Row>
+          <Cell head />
+          <Cell head>Buy</Cell>
+          <Cell head>Make</Cell>
+          <Cell head>Sell</Cell>
+        </Row>
+      </Head>
+      <Body>
+        <Row>
+          <Cell icon="money" />
+          <Cell topLeft bottomLeft />
+          <Cell carryOver />
+          <Cell topRight bottomRight final />
+        </Row>
+      </Body>
+      <Body>
+        <Row>
+          <Cell icon="time" />
+          <Cell topLeft bottomLeft />
+          <Cell />
+          <Cell topRight bottomRigh final />
+        </Row>
+      </Body>
+      <Body>
+        <Row>
+          <Cell icon="A" />
+          <Cell topLeft />
+          <Cell />
+          <Cell topRight final carryOver />
+        </Row>
+        <Row>
+          <Cell icon="B" />
+          <Cell />
+          <Cell />
+          <Cell final carryOver />
+        </Row>
+        <Row>
+          <Cell icon="C" />
+          <Cell />
+          <Cell />
+          <Cell final carryOver />
+        </Row>
+        <Row>
+          <Cell icon="D" />
+          <Cell bottomLeft />
+          <Cell />
+          <Cell bottomRight final carryOver />
+        </Row>
+      </Body>
+      <Body>
+        <Row>
+          <Cell icon="AB" />
+          <Cell topLeft carryOver />
+          <Cell />
+          <Cell topRight final />
+        </Row>
+        <Row>
+          <Cell icon="ABC" />
+          <Cell carryOver />
+          <Cell />
+          <Cell final />
+        </Row>
+        <Row>
+          <Cell icon="ABCD" />
+          <Cell bottomLeft carryOver />
+          <Cell />
+          <Cell bottomRight final />
+        </Row>
+      </Body>
     </div>
   );
 }
