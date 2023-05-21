ff404a3e589b578a7d6ca8ba28cd4b521e27890d
Tommy <tommy@iamnotagoodartist.com>
2023-05-21 10:08:18 -0700

    Stub Rules

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = withMDX(nextConfig);
