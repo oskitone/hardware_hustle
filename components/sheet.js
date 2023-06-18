c8c35c4c7eeda71c1f157a6368942c66fd9ef0a9
Tommy <tommy@iamnotagoodartist.com>
2023-06-18 11:15:31 -0700

    Differentiate legal/letter Sheet sizes

--- components/sheet.js
+++ components/sheet.js
@@ -2,17 +2,24 @@ import classnames from "classnames";
 
 import styles from "@/styles/Sheet.module.css";
 
-function Sheet({ children, landscape }) {
+function Sheet({ children, landscape, size }) {
   return (
     <div
       className={classnames(styles.sheet, {
         [styles.landscape]: landscape,
+        [styles.legal]: size == "legal",
+        [styles.letter]: size == "letter",
       })}
     >
       {children}
     </div>
   );
 }
+
+Sheet.defaultProps = {
+  landscape: false,
+  size: "legal",
+};
 
 export const Front = Sheet;
 export const Back = Sheet;
