oskitone/hardware_hustle

commit f57c5c55baebc53ef1499677d4b143452b6a213e

Allow per-row column props, not per-body

components/turn.js
@@ -119,46 +119,62 @@ export function Cell({
 function Turn({ id, data, isFinalTurn, className, suppliedColumns }) {
   const COLUMNS = ["AM", "Buy", "Make", "Sell", "PM"];
 
+  const PARTS_COLUMNS = [
+    {},
+    { prefix: PLUS },
+    { prefix: MINUS },
+    { carryOver: true },
+    { prefix: EQUALS },
+  ];
+  const WIDGETS_COLUMNS = [
+    {},
+    { carryOver: true },
+    { prefix: PLUS },
+    { prefix: MINUS },
+    { prefix: EQUALS },
+  ];
+
   const BODIES = [
     {
       rows: ["money"],
-      columns: [
-        {},
-        { prefix: MINUS },
-        { carryOver: true },
-        { prefix: PLUS },
-        { prefix: EQUALS },
-      ],
+      columns: {
+        money: [
+          {},
+          { prefix: MINUS },
+          { carryOver: true },
+          { prefix: PLUS },
+          { prefix: EQUALS },
+        ],
+      },
     },
     {
       rows: ["A", "B", "C", "D"],
-      columns: [
-        {},
-        { prefix: PLUS },
-        { prefix: MINUS },
-        { carryOver: true },
-        { prefix: EQUALS },
-      ],
+      columns: {
+        A: PARTS_COLUMNS,
+        B: PARTS_COLUMNS,
+        C: PARTS_COLUMNS,
+        D: PARTS_COLUMNS,
+      },
     },
     {
       rows: ["AB", "ABC", "ABCD"],
-      columns: [
-        {},
-        { carryOver: true },
-        { prefix: PLUS },
-        { prefix: MINUS },
-        { prefix: EQUALS },
-      ],
+      columns: {
+        AB: WIDGETS_COLUMNS,
+        ABC: WIDGETS_COLUMNS,
+        ABCD: WIDGETS_COLUMNS,
+      },
     },
     {
       rows: ["opportunity"],
-      columns: [
-        {},
-        { prefix: MINUS },
-        { prefix: MINUS },
-        { prefix: MINUS },
-        { prefix: EQUALS },
-      ],
+      columns: {
+        opportunity: [
+          {},
+          { prefix: MINUS },
+          { prefix: MINUS },
+          { prefix: MINUS },
+          { prefix: EQUALS },
+        ],
+      },
     },
   ];
 
@@ -187,7 +203,7 @@ function Turn({ id, data, isFinalTurn, className, suppliedColumns }) {
               <Row key={rowI}>
                 <Cell icon={rowIcon} />
 
-                {body.columns.map((column, columnI) => {
+                {body.columns[rowIcon].map((column, columnI) => {
                   const isFirstColumn = columnI == 0;
                   const isLastColumn = columnI == body.columns.length - 1;
                   const isSupplied = (suppliedColumns[rowIcon] || [])[columnI];

commit 5a6e5774e750a0180a6f46ab8f130762495c3689

Fix right Turn Cell radii
Broken in f57c5c55baebc53ef1499677d4b143452b6a213e

components/turn.js
@@ -205,7 +205,8 @@ function Turn({ id, data, isFinalTurn, className, suppliedColumns }) {
 
                 {body.columns[rowIcon].map((column, columnI) => {
                   const isFirstColumn = columnI == 0;
-                  const isLastColumn = columnI == body.columns.length - 1;
+                  const isLastColumn =
+                    columnI == body.columns[rowIcon].length - 1;
                   const isSupplied = (suppliedColumns[rowIcon] || [])[columnI];
                   const isEmpty = isUndefined(data[rowIcon][columnI]);
 