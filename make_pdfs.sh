f949c080219374880880651a36e3cd42737ba6d0
Tommy <tommy@iamnotagoodartist.com>
2023-10-01 20:52:43 -0700

    Confirm URL is reachable before making PDFs

--- make_pdfs.sh
+++ make_pdfs.sh
@@ -8,6 +8,8 @@ set -o errtrace
 
 chrome="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
 
+url="http://localhost:3000"
+
 timestamp=$(git log -n1 --date=unix --format="%ad")
 commit_hash=$(git log -n1 --format="%h")
 stub="hardware_hustle-${commit_hash}"
@@ -15,19 +17,29 @@ dir="output/${timestamp}-${commit_hash}"
 
 mkdir -pv "${dir}" >/dev/null
 
+function confirm_url() {
+    wget -q --spider "${url}" || {
+        echo "${url} is not available"
+        echo "Try 'npm run dev'"
+        exit
+    }
+}
+
 function export_stl() {
     path="$1"
     filename="${dir}/${stub}-${path}.pdf"
 
-    echo "Exporting $filename..."
+    echo "Exporting ${filename}"
 
     "$chrome" \
         --headless \
         --print-to-pdf="${filename}" \
         --virtual-time-budget=50000 \
-        "http://localhost:3000/${path}" \
+        "${url}/${path}" \
         2>/dev/null
 }
+
+confirm_url
 
 export_stl letter
 export_stl legal
