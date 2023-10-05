e032ee5a706bf459c4aa5aa8b49a5d578483989b
Tommy <tommy@iamnotagoodartist.com>
2023-10-05 13:58:52 -0700

    Who made this?!

--- components/wordmark.js
+++ components/wordmark.js
@@ -5,6 +5,7 @@ import styles from "@/styles/Wordmark.module.css";
 function Wordmark({ className, draftId }) {
   return (
     <div className={classnames(styles.wordmark, className)}>
+      <div className={styles.oskitones}>Oskitone's</div>
       <h1>Hardware Hustle</h1>
       <div className={styles.rev}>{draftId}</div>
     </div>
