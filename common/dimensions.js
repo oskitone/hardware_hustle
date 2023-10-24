888b58beea94fe1e9e2cde68a1900c1b685258ce
Tommy <tommy@iamnotagoodartist.com>
2023-10-23 17:18:49 -0700

    DRY dimensions

--- common/dimensions.js
+++ common/dimensions.js
@@ -1,5 +1,3 @@
-// TODO: DRY against globals.css?
-
 export const legal_sheet_width = "8.5in";
 export const legal_sheet_height = "14in";
 
@@ -8,3 +6,6 @@ export const letter_sheet_height = "11in";
 
 export const page_width = "8in";
 export const page_height = "6in";
+
+export const letter_page_width = "5.5in";
+export const letter_page_height = "7.5in";
--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,39 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+
+import {
+  legal_sheet_width,
+  legal_sheet_height,
+  letter_sheet_width,
+  letter_sheet_height,
+  page_width,
+  page_height,
+  letter_page_width,
+  letter_page_height,
+} from "common/dimensions";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <style>{`
+          :root {
+            --legal-sheet-width: ${legal_sheet_width};
+            --legal-sheet-height: ${legal_sheet_height};
+
+            --letter-sheet-width: ${letter_sheet_width};
+            --letter-sheet-height: ${letter_sheet_height};
+
+            --page-width: ${page_width};
+            --page-height: ${page_height};
+
+            --letter-page-width: ${letter_page_width};
+            --letter-page-height: ${letter_page_height};
+        `}</style>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
